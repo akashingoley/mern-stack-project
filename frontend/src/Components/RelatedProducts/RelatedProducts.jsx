e24435bb272b9596407cd733806924ef84d635f0
Akash Ingoley <akashingoley@gmail.com>
2024-02-28 16:04:47 +0530

    Related products changes

--- frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -6,20 +6,16 @@ import { ShopContext } from '../../Context/ShopContext'
 
 const RelatedProducts = (props) => {
   const {product} = props;
-  const { all_product } = useContext(ShopContext)
-  // console.log("all products: ",all_product[4, 5]);
+  const { all_product } = useContext(ShopContext);
+  let relatedProd = all_product.filter((e) => e.category === product.category);
 
   return (
     <div className='relatedproducts'>
         <h1>Related Products</h1>
         <hr/>
         <div className="relatedproducts-item">
-            { all_product.map((item, i) => {
-              if(product.category === item.category){
-                return <Item key = { i } id = { item.id } name = { item.name } image = { item.image } new_price = { item.new_price } old_price = { item.old_price }/>
-              }else{
-                return null;
-              }
+            { relatedProd.slice(0,4).map((item, i) => {
+              return <Item key = { i } id = { item.id } name = { item.name } image = { item.image } new_price = { item.new_price } old_price = { item.old_price }/>
             })}
         </div>
     </div>
